94e70687914e7ec8b46b67b6b30e34dc08105c93
aitouahmansalah <salahaitouahman1@gmail.com>
2024-05-15 14:55:50 +0100

    S3 func by salah

--- src/main/java/com/rentalCar/CarRentalApplication.java
+++ src/main/java/com/rentalCar/CarRentalApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarRentalApplication {
+	//main class
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarRentalApplication.class, args);
